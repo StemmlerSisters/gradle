520cb65474ee77d468b2b988d8c3dca4175f120d
Mikhail Lopatkin <mlopatkin@gradle.com>
2026-04-28 19:26:03 +0200

    Retire pre-stable CC task access warnings
    These were introduced back in Gradle 8.1 for some old-ish tests, but
    these have been fixed since then.

--- testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/GradleVersionsPluginSmokeTest.groovy
+++ testing/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/GradleVersionsPluginSmokeTest.groovy
@@ -60,8 +60,6 @@ class GradleVersionsPluginSmokeTest extends AbstractPluginValidatingSmokeTest {
 
         when:
         def runner = runner('dependencyUpdates', '-DoutputFormatter=txt')
-            // TODO: com.github.benmanes.gradle.versions.updates.DependencyUpdates plugin triggers dependency resolution at execution time
-            .withJvmArguments("-Dorg.gradle.internal.configuration-cache.task-execution-access-pre-stable=true")
 
         runner
 
